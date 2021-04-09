32db5dddd683492584f736bf03e79cd38af8e30a
CaseyCameron <mcasey.cameron@gmail.com>
2021-04-09 09:19:03 -0700

    maps setup

---
+++ map/map.js
@@ -0,0 +1,10 @@
+import quests from '../data/data.js';
+
+const mapSection = document.querySelector('section');
+
+for (let quest of quests){
+    const anchorTag = document.createElement('a');
+    anchorTag.textContent = quest.title;
+    anchorTag.href = `../quest/?=${quest.id}`;
+    mapSection.append(anchorTag);
+}
